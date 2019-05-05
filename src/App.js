1e197552934a6128a9d8cc82095e41da4ff3d4b9
Danny Yen <danny.y.yen@gmail.com>
2019-05-05 22:32:03 +0300

    add gitIgnore

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ export default class App extends Component {
   componentDidMount() {
    this.getData();
   }  
-  useAxios = (query = "baby") =>{
+  getData = () => {
     axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`)
       .then(response => {
         //axios has its own callback data property 
