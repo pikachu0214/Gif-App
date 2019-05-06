02041993859914d206c76171bc31cf9b11827abd
pikachu0214 <danny.y.yen@gmail.com>
2019-05-06 16:09:50 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ export default class App extends Component {
    this.getData();
   }  
   getData = () => {
-    axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`)
+    axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=YOUR_API_KEY`)
       .then(response => {
         //axios has its own callback data property 
         this.setState({ 
